5962071c16972c023db1b58bb0a656e0086fb751
mansi0575 <mansimhatre0575@gmail.com>
2021-05-07 22:04:34 +0530

    class Player   attr_accessor :name ,:total ,:boundaries, :no_of_balls, :dot_ball   def initialize(name)     @name = name     @total = 0     @boundaries = 0     @no_of_balls = 0     @dot_ball = 0
      end
     def add_run(run)
         if run.to_i < 8
         @total = @total.to_i + run.to_i
         end
    
          if run.to_i > 7
            puts "invalid score"
          else
          puts "#{run}"
          end
    
          if run.to_i >= 4
          @boundaries +=1
         end
    
       #    @no_of_balls = add_run.length
    
          if run.to_i === 0
            @dot_ball += 1
          end
    end
    
       def total_score
         @total
       end
    
       def boundaries
         @boundaries
       end
    
       def details
         puts "Name:#{@name}, RUN:#{@total}, Boundaries:#{@boundaries}"
    
       end
    
        def no_of_balls
        #  @no_of_balls
        end
    
        def no_of_dot_balls
          puts "Name:#{@name} ,numbar of dot balls #{@dot_ball}"
        end
    
      def run_at_ball
      end
    
    end
    
    player1 = Player.new("Dhoni")
    player2 = Player.new("Sachin" )
    player1.add_run("0")
    player1.add_run("6")
    player1.add_run("1")
    player2.add_run("6")
    player2.add_run("1")
    
    puts player1.total_score
    puts player2.total_score
    puts player1.boundaries
    puts player2.boundaries
    puts player1.details
    puts player2.details
    puts player1.no_of_dot_balls
    updated file

--- finalplayer.rb
+++ finalplayer.rb
@@ -1,37 +1,65 @@
 class Player
-  attr_accessor :name ,:total ,:boundaries 
+  attr_accessor :name ,:total ,:boundaries, :no_of_balls, :dot_ball
+  
   def initialize(name)
     @name = name
     @total = 0
     @boundaries = 0
+    @no_of_balls = 0
+    @dot_ball = 0
+  
+  end
+  def add_run(run)
+     if run.to_i < 8
+     @total = @total.to_i + run.to_i
+     end
+ 
+     if run.to_i > 7
+        puts "invalid score"
+     else
+      puts "#{run}"
+     end
+ 
+     if run.to_i >= 4
+      @boundaries +=1
+     end
+
+   # @no_of_balls = add_run.length
+
+     if run.to_i === 0
+        @dot_ball += 1
+     end
   end
 
-   def add_run(run)
-     @total = @total.to_i + run.to_i
-      if run.to_i >= 4
-       @boundaries +=1
-     end
-   end
-
-   def total_score
+  def total_score
      @total
-   end
+  end
 
-   def boundaries
+  def boundaries
      @boundaries
-   end
+  end
 
-   def details
+  def details
      puts "Name:#{@name}, RUN:#{@total}, Boundaries:#{@boundaries}"
-     
-   end
+  end
+
+  def no_of_balls
+    #  @no_of_balls
+  end
+
+  def no_of_dot_balls
+      puts "Name:#{@name} ,numbar of dot balls #{@dot_ball}"
+  end
+
+  def run_at_ball
+  end
      
 end
 
 
 player1 = Player.new("Dhoni")
 player2 = Player.new("Sachin" )
-player1.add_run("4")
+player1.add_run("0")
 player1.add_run("6")
 player1.add_run("1")
 player2.add_run("6")
@@ -43,3 +71,4 @@ puts player1.boundaries
 puts player2.boundaries
 puts player1.details
 puts player2.details
+puts player1.no_of_dot_balls
