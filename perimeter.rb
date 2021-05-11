f3a9ba97f2ee9297553b7b0eebbf7cb7f6017bac
mansi0575 <mansimhatre0575@gmail.com>
2021-05-11 14:12:32 +0530

    perimeter

--- perimeter.rb
+++ perimeter.rb
@@ -10,12 +10,20 @@ class Circle
    2 * @@pi * @radius
  end
 
+  def area_circle
+    @@pi * @radius * @radius
+  end
+
 end
 
 class Square
   attr_accessor :a              # a = side of the square
   def initialize(a)
     @a = a
+  end
+
+  def area_square
+    @a * @a
   end
   
   def p_square
@@ -26,7 +34,9 @@ end
 
 p = Circle.new (2)
 puts "Perimeter of the Circle #{p.p_circle}"
+puts " Area of the Circle #{p.area_circle}"
   
 s = Square.new (5)
 puts "Perimeter of the Square #{s.p_square}"
+puts "Area of the Square #{s.area_square}"
     
